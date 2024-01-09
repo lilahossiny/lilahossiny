4d4bbdb5e1bf4dac4020d262dc717935e7ba7a48
lilahossiny <156114718+lilahossiny@users.noreply.github.com>
2024-01-09 23:41:35 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+window.setTimeout(function() {
+    $(".alert").fadeTo(500, 0).slideUp(500, function(){
+        $(this).remove(); 
+    });
+}, 5000);
+
 (function($angular, _, Hammer) {
 	'use strict';
 	
